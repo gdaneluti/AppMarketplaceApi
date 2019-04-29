0b0a96797c428d585fb94683523000d36b37c7fc
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-29 19:03:18 -0300

    Configuracao de Exception Handler

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const validate = require('express-validation')
-
+const handle = require('express-async-handler')
 const routes = express.Router()
 
 const controllers = require('./app/controllers')
@@ -11,12 +11,12 @@ const authMiddleware = require('./app/middlewares/auth')
 routes.post(
   '/users',
   validate(validators.User),
-  controllers.UserController.store
+  handle(controllers.UserController.store)
 )
 routes.post(
   '/sessions',
   validate(validators.Session),
-  controllers.SessionController.store
+  handle(controllers.SessionController.store)
 )
 
 // Toda rota a partir daqui para baixo irá utilizar o middleware de auth
@@ -25,11 +25,19 @@ routes.use(authMiddleware)
 /**
  * Ads
  */
-routes.get('/ads', controllers.AdController.index)
-routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
-routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
-routes.delete('/ads/:id', controllers.AdController.destroy)
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
 /**
  * Purchase
@@ -38,7 +46,7 @@ routes.delete('/ads/:id', controllers.AdController.destroy)
 routes.post(
   '/purchases',
   validate(validators.Purchase),
-  controllers.PurchaseController.store
+  handle(controllers.PurchaseController.store)
 )
 
 module.exports = routes
