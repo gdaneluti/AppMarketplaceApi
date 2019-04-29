510eddf32dcc93a32d0c0770fa01dfe3aeea695d
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-29 18:47:49 -0300

    Adicionando validacao nas requisiçoes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
+
 const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 // Toda rota a partir daqui para baixo irá utilizar o middleware de auth
 routes.use(authMiddleware)
@@ -16,14 +27,18 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * Purchase
  */
 
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
