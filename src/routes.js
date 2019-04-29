aa804d10eee2218d1bde8f88cc8f4f3f63382cc5
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-29 14:33:19 -0300

    Instalacao e configuracao do require-dir para generalizar a importacao dos controller no routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,11 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controller = require('./app/controllers')
 const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controller.UserController.store)
+routes.post('/sessions', controller.SessionController.store)
 
 routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 module.exports = routes
