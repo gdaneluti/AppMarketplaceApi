02511e5ac20a86181620ace8b2bfef05151e4a5e
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-29 13:27:52 -0300

    Configuracao de conexao com mongoDB

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
@@ -6,10 +8,17 @@ class App {
     this.isDev = process.env.NODE_ENV !== 'production'
 
     this.middlewares()
+    this.database()
     this.routes()
   }
 
-  middleswares () {
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
+  }
+  middlewares () {
     this.express.use(express.json())
   }
 
